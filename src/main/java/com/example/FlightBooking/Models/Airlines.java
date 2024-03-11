31225480981c2eba82eeaaa37b1f685724c0b231
LeDangThuong <ledangthuongsp@gmail.com>
2024-03-11 22:06:40 +0700

    Xây dựng cơ bản hoàn chỉnh table

--- src/main/java/com/example/FlightBooking/Models/Airlines.java
+++ src/main/java/com/example/FlightBooking/Models/Airlines.java
@@ -1,4 +1,18 @@
 package com.example.FlightBooking.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.Generated;
+
+@Data
+@Entity
+@Table(name = "airlines")
 public class Airlines {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
 }
