64b20dce8baa5ca50eb6d64022b968ba14fcafb4
LeDangThuong <ledangthuongsp@gmail.com>
2024-03-11 21:43:58 +0700

    Test lại commit mới pull về

--- src/main/java/com/example/FlightBooking/Models/Airports.java
+++ src/main/java/com/example/FlightBooking/Models/Airports.java
@@ -1,4 +1,17 @@
 package com.example.FlightBooking.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "airports")
 public class Airports {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
