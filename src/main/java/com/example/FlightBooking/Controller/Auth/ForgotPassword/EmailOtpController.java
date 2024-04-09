dbfad0eaf5ecdd2cea068d6f0aa5a1ec6578d99b
Lê Đăng Thương <ledangthuongsp@gmail.com>
2024-04-10 02:44:40 +0700

    Sửa nốt cái CORS rồi đi ngủ nè, mai fix tiếp

--- src/main/java/com/example/FlightBooking/Controller/Auth/ForgotPassword/EmailOtpController.java
+++ src/main/java/com/example/FlightBooking/Controller/Auth/ForgotPassword/EmailOtpController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Controller
 @CrossOrigin (value = "*")
 public class EmailOtpController {
     @Autowired
--- src/main/java/com/example/FlightBooking/Controller/Auth/ForgotPassword/ResetPasswordController.java
+++ src/main/java/com/example/FlightBooking/Controller/Auth/ForgotPassword/ResetPasswordController.java
@@ -6,12 +6,14 @@ import com.example.FlightBooking.Services.AuthJWT.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Controller
 @CrossOrigin (value = "*")
 public class ResetPasswordController {
     @Autowired
