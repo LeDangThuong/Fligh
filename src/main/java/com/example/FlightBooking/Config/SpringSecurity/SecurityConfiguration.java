e8042b2d1c2ab4807a221b9e75375cd42927d926
Lê Đăng Thương <ledangthuongsp@gmail.com>
2024-04-10 01:00:02 +0700

    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Allow these methods         configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type", "Accept", "Access-Control-Allow-Origin")); // Allow these headers         configuration.setExposedHeaders(List.of("Authorization", "Cache-Control", "Content-Type", "Accept", "Access-Control-Allow-Origin"));

--- src/main/java/com/example/FlightBooking/Config/SpringSecurity/SecurityConfiguration.java
+++ src/main/java/com/example/FlightBooking/Config/SpringSecurity/SecurityConfiguration.java
@@ -56,7 +56,14 @@ public class SecurityConfiguration {
                         .anyRequest().authenticated()) // All other requests need authentication
                 .headers(headers -> headers
                         .contentSecurityPolicy(csp ->
-                                csp.policyDirectives("default-src 'self'; script-src 'self' https://flightbookingbe-production.up.railway.app/swagger-ui/index.html;")
+                                csp.policyDirectives("default-src 'self';" +
+                                        "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://flightbookingbe-production.up.railway.app;" +
+                                        "style-src 'self' 'unsafe-inline';" +
+                                        "img-src 'self' data:;" +
+                                        "connect-src 'self' https://flightbookingbe-production.up.railway.app;" +
+                                        "font-src 'self';" +
+                                        "frame-src 'self';" +
+                                        "child-src 'self';")
                         )
                 )
                 .authenticationProvider(authenticationProvider)
