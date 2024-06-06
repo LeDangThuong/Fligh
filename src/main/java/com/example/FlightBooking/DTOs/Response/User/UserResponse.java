274c4a350e850c7eca3649c7e89b92babd4719f1
Lê Đăng Thương <ledangthuongsp@gmail.com>
2024-06-07 00:59:34 +0700

    Cập nhật lại API get User by token + username

--- src/main/java/com/example/FlightBooking/DTOs/Response/User/UserResponse.java
+++ src/main/java/com/example/FlightBooking/DTOs/Response/User/UserResponse.java
@@ -1,14 +1,25 @@
 package com.example.FlightBooking.DTOs.Response.User;
 
+import com.example.FlightBooking.Enum.Roles;
 import com.example.FlightBooking.Models.Users;
 
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.sql.Timestamp;
+
 @Getter
 @Setter
 @Data
 public class UserResponse {
-    private Users user;
+    private Long id;
+    private String username;
+    private String email;
+    private String fullName;
+    private Roles role;
+    private String avatarUrl;
+    private Timestamp dayOfBirth;
+    private String phoneNumber;
+    private String address;
 }
